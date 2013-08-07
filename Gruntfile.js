80d50100efc2f1bde5fcdb2e6f31e6654b22b4d3
Federico Bana <me@banafederico.com>
2013-08-07 13:36:26 -0300

    Compile coffeescript with grunt-contrib-coffee.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,20 +4,32 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
 
   grunt.initConfig({
     connect: {
       server: {
         options: {
           port: 9000,
-          host: '0.0.0.0'
+          host: '0.0.0.0',
+          keepalive: true
         }
       }
-    }
+    }, /* connect */
+    coffee: {
+      compile: {
+        files: {
+          'js/app.js': ['app/coffee/{,*/}*.coffee']
+        }
+      }
+    } /* coffee */
   });
 
   grunt.registerTask('server', 'Does all the grunt work', function () {
-    grunt.task.run(['connect:server']);
+    grunt.task.run([
+      'coffee',
+      'connect:server'
+    ]);
   });
 
   grunt.registerTask('default', ['server']);
