da2477830eb3b24d0979d7483a3a9326c6516b3f
Federico Bana <me@banafederico.com>
2013-08-07 13:16:32 -0300

    Use grunt-contrib-connect to start the server.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,24 @@
 /*global module:false*/
 module.exports = function(grunt) {
-  require(process.env['LINEMAN_MAIN']).config.grunt.run(grunt);
+  //require(process.env['LINEMAN_MAIN']).config.grunt.run(grunt);
+
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  grunt.initConfig({
+    connect: {
+      server: {
+        options: {
+          port: 9000,
+          host: '0.0.0.0'
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('server', 'Does all the grunt work', function () {
+    grunt.task.run(['connect:server']);
+  });
+
+  grunt.registerTask('default', ['server']);
 };
