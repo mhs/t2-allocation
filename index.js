838e045497896d1b064083a0135b7a71a8395986
Tomek Maszkowski <pair+tomek@neo.com>
2013-10-03 11:28:44 +0800

    commit d4a4dba300acfee405f2fb1b5bd0dab70011052a Merge: 8a4b4d3 f2f161e Author: Mike Doel <mikedoel@neo.com> Date:   Tue Oct 1 08:18:30 2013 -0700
        Merge pull request #31 from neo/issue-5-filter-by-office
    
        Filter by office

--- index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express')
-  , app = express();
-
-app.use(express.static(__dirname));
-
-app.listen(process.env.PORT || 5000, function () {
-  console.log('express server started.');
-});
