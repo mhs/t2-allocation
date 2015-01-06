f92df7cc68c187048804c17348be3385f5714923
Sara Gibbons <saragibby@gmail.com>
2015-01-06 11:49:21 -0500

    fix the query date picker after ember upgrade

--- Brocfile.js
+++ Brocfile.js
@@ -1,11 +1,22 @@
 /* global require, module */
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var mergeTrees = require('broccoli-merge-trees');
+var pickFiles = require('broccoli-static-compiler');
 
 var app = new EmberApp();
 
 app.import('bower_components/moment/moment.js')
 app.import('bower_components/jquery-ui/jquery-ui.js')
+app.import('bower_components/jquery-ui/themes/redmond/jquery-ui.css')
+
+var extraAssets = pickFiles('bower_components/jquery-ui/themes/redmond/images', {
+  srcDir: '/',
+  files: ['*.gif', '*.png'],
+  destDir: '/assets/images'
+});
+
+
 // Use `app.import` to add additional libraries to the generated
 // output files.
 //
@@ -19,4 +30,4 @@ app.import('bower_components/jquery-ui/jquery-ui.js')
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-module.exports = app.toTree();
+module.exports = mergeTrees([app.toTree(), extraAssets], {overwrite: true});
