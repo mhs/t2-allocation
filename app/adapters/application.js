73a151f23fe853157207d51278a70ee78145e045
Chris Westra <cwestra@gmail.com>
2017-04-07 10:46:50 -0400

    Use active-model-adapter addon

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,9 +1,4 @@
-// TODO: This file was created by bulk-decaffeinate.
-// Sanity-check the conversion and remove this comment.
-import DS from "ember-data";
 import ENV from "t2-allocation/config/environment";
+import ActiveModelAdapter from 'active-model-adapter';
 
-let ApplicationAdapter = DS.ActiveModelAdapter.extend({
-  host: ENV.API_BASEURL});
-
-export default ApplicationAdapter;
+export default ActiveModelAdapter.extend({host: ENV.API_BASEURL});
--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,5 +1,2 @@
-// TODO: This file was created by bulk-decaffeinate.
-// Sanity-check the conversion and remove this comment.
-import DS from "ember-data";
-let ApplicationSerializer = DS.ActiveModelSerializer.extend();
-export default ApplicationSerializer;
+import { ActiveModelSerializer } from 'active-model-adapter';
+export default ActiveModelSerializer.extend();
